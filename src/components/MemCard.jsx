5a93765ebb83f1d9013670e8a6801cbbfe8128c0
ArkPac97 <arekpachuta97@gmail.com>
2024-05-03 12:43:31 +0200

    Creating funcionality of saving votes using useState and localStorage

--- src/components/MemCard.jsx
+++ src/components/MemCard.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import memData from '../data/mems.json';
+import React, { useState, useEffect } from 'react';
+import memsData from '../data/mems.json';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons';
@@ -7,20 +7,41 @@ import { faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons';
 library.add(faThumbsUp, faThumbsDown);
 
 function MemCard() {
-  const [upvotes, setUpvotes] = useState(memData[0].upvotes);
-  const [downvotes, setDownvotes] = useState(memData[0].downvotes);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [memData, setMemData] = useState(() => {
+    const localData = localStorage.getItem('memData');
+    return localData ? JSON.parse(localData) : memsData;
+  });
   const [bounceUp, setBounceUp] = useState(false);
   const [bounceDown, setBounceDown] = useState(false);
   const [favoriteClicked, setFavoriteClicked] = useState(false);
 
+  useEffect(() => {
+    localStorage.setItem('memData', JSON.stringify(memData));
+  }, [memData]);
+
+  const triggerVote = (type) => {
+    const updatedMems = memData.map((mem, index) => {
+      if (index === currentIndex) {
+        if (type === 'up') {
+          return { ...mem, upvotes: mem.upvotes + 1 };
+        } else {
+          return { ...mem, downvotes: mem.downvotes + 1 };
+        }
+      }
+      return mem;
+    });
+    setMemData(updatedMems);
+  };
+
   const triggerBounceUp = () => {
-    setUpvotes(upvotes + 1);
+    triggerVote('up');
     setBounceUp(true);
     setTimeout(() => setBounceUp(false), 1000);
   };
 
   const triggerBounceDown = () => {
-    setDownvotes(downvotes + 1); 
+    triggerVote('down');
     setBounceDown(true);
     setTimeout(() => setBounceDown(false), 1000);
   };
@@ -30,25 +51,32 @@ function MemCard() {
     setTimeout(() => setFavoriteClicked(false), 100);
   };
 
+  const goToNextMem = () => {
+    setCurrentIndex((currentIndex + 1) % memData.length);
+  };
+
+  const goToPrevMem = () => {
+    setCurrentIndex(currentIndex === 0 ? memData.length - 1 : currentIndex - 1);
+  };
 
   return (
     <div className='mem__card'>
-      <h1 className='mem__card-title'>{memData[0].title}</h1>
+      <h1 className='mem__card-title'>{memData[currentIndex].title}</h1>
       <div className='mem__card-mem'>
-        <img className='mem__slide-left' src="./arrow_left.png" alt="arrow_left" />
-        <img src={memData[0].img} alt="Meme" />
-        <img className='mem__slide-right' src="./arrow_right.png" alt="arrow_right" />
+        <img className='mem__slide-left' src="./arrow_left.png" alt="arrow_left" onClick={goToPrevMem} />
+        <img src={memData[currentIndex].img} alt="Meme" />
+        <img className='mem__slide-right' src="./arrow_right.png" alt="arrow_right" onClick={goToNextMem} />
       </div>
       <button className='favorite__btn' onClick={toggleFavorite}>
         <img className={`favorite__btn-icon ${favoriteClicked ? 'scale-down' : ''}`} src="./favorite.png" alt="Favorite button" />
       </button>
       <div className='vote__btns'>
         <button className='vote__btns-up vote__btn' onClick={triggerBounceUp}>
-          {upvotes}
+          {memData[currentIndex].upvotes}
           <FontAwesomeIcon icon="fa-solid fa-thumbs-up" className={`${bounceUp ? 'bounce' : ''} icon-margin`} />
         </button>
         <button className='vote__btns-down vote__btn' onClick={triggerBounceDown}>
-          {downvotes}
+          {memData[currentIndex].downvotes}
           <FontAwesomeIcon icon="fa-solid fa-thumbs-down" className={`${bounceDown ? 'bounce' : ''} icon-margin`} />
         </button>
       </div>
