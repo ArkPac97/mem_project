f53573962434a543a9bae10fde8df1713c5b35ea
ArkPac97 <arekpachuta97@gmail.com>
2024-05-06 21:12:25 +0200

    Changing logic of NavBar component buttons rendering in order to deal with some new styling showing more clearly which view is active

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,45 @@
 // src/components/NavigationBar.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function NavBar() {
   const navigate = useNavigate();
+  const [activeButton, setActiveButton] = useState('/');
+
+  const handleNavigation = (path) => {
+    navigate(path);
+    setActiveButton(path);
+  };
+
+  const renderButtonLabel = (label, isActive) => (
+    <span className="button-label">
+      {label.split('').map((char, index) => (
+        <span key={index} className={`letter ${isActive ? `active-${index}` : ''}`}>{char}</span>
+      ))}
+    </span>
+  );
+
+  const buttons = [
+    { path: '/', label: 'home' },
+    { path: '/hot', label: 'hot' },
+    { path: '/regular', label: 'regular' },
+    { path: '/mybest', label: 'mybest' }
+  ];
 
   return (
     <nav className='nav__bar'>
-      <div className='nav__bar-home' onClick={() => navigate("/")}>
+      <div className='nav__bar-home' onClick={() => handleNavigation("/")}>
         <img className='nav__bar-img' src="./header_icon.png" alt="Home Icon" />
         <h2 className='nav__bar-header'>mem serwis</h2>
       </div>
       <div className='nav__bar-navigation_btns'>
-        <button className='nav__bar-navigation_btn home__btn' onClick={() => navigate("/")}>home</button>
-        <button className='nav__bar-navigation_btn hot__btn' onClick={() => navigate("/hot")}>hot</button>
-        <button className='nav__bar-navigation_btn regular__btn' onClick={() => navigate("/regular")}>regular</button>
-        <button className='nav__bar-navigation_btn mybest__btn' onClick={() => navigate("/mybest")}>my best</button>
+        {buttons.map((btn, idx) => (
+          <button key={idx}
+                  className={`nav__bar-navigation_btn ${btn.label.replace(' ', '__')}__btn ${activeButton === btn.path ? 'active' : ''}`}
+                  onClick={() => handleNavigation(btn.path)}>
+            {renderButtonLabel(btn.label, activeButton === btn.path)}
+          </button>
+        ))}
       </div>
     </nav>
   );
