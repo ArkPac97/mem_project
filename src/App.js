d56bc78c1a7d256300ef693b34e92fcfd6c887c4
ArkPac97 <arekpachuta97@gmail.com>
2024-05-06 21:24:19 +0200

    Changing App.js logic in order to deal with animation of trepassing between views, installing librar react-trasitions-group and adding some styline of animation of trepassing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 // src/App.js
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import NavBar from './components/NavBar';
 import { Home } from "./views/Home";
 import { HotMems } from "./views/HotMems";
 import { RegularMems } from "./views/RegularMems";
 import { MyBestMems } from "./views/MyBestMems";
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
+function AnimatedRoutes() {
+  const location = useLocation(); // Hook
+
+  return (
+    <TransitionGroup component={null}>
+      <CSSTransition
+        key={location.key}
+        timeout={300} 
+        classNames="fade"
+      >
+        <Routes location={location}>
+          <Route path="/" element={<Home />} />
+          <Route path="/hot" element={<HotMems />} />
+          <Route path="/regular" element={<RegularMems />} />
+          <Route path="/mybest" element={<MyBestMems />} />
+        </Routes>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/hot" element={<HotMems />} />
-          <Route path="/regular" element={<RegularMems />} />
-          <Route path="/mybest" element={<MyBestMems />} />
-        </Routes>
+        <AnimatedRoutes />
       </div>
     </BrowserRouter>
   );
